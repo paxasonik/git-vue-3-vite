151174c9878db038b3bac6f84ce1adeea3bf31fa
paxasonik <paxasonk@mail.ru>
2022-11-08 19:02:52 +0300

    add method delete post

--- src/pages/PostsList.vue
+++ src/pages/PostsList.vue
@@ -12,6 +12,9 @@
       <div class="">
         {{ post.body }}
       </div>
+      <button class="" @click="deletePost(post.id)">
+        Удалить
+      </button>
     </div>
   </div>
 </template>
@@ -27,14 +30,29 @@ export default {
     }
   },
   mounted() {
-    axios.get('https://jsonplaceholder.typicode.com/posts')
-        .then(res => {
-          this.postList = res.data
-          // console.log(res)
-        })
-        .catch(err => {
-          console.log(err)
-        })
+    this.getPosts()
+  },
+  methods: {
+    getPosts() {
+      axios.get('https://jsonplaceholder.typicode.com/posts')
+      .then(res => {
+        this.postList = res.data
+        // console.log(res)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+    },
+    deletePost(id) {
+      axios.delete(`https://jsonplaceholder.typicode.com/posts/${id}`)
+      .then(res => {
+        this.postList = this.postList.filter(post => post.id !== id)
+        // console.log(res)
+      })
+      .catch(error => [
+          console.log(error)
+      ])
+    }
   },
 }
 </script>
