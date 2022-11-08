4993f5da6aad0f758130dc1b7f46150e9be25a47
paxasonik <paxasonk@mail.ru>
2022-11-08 20:03:21 +0300

    project style

--- src/pages/Main.vue
+++ src/pages/Main.vue
@@ -1,6 +1,9 @@
 <template>
-  <h1>Главная</h1>
-  <router-link :to="{name: 'PostsList'}">Список постов</router-link>
+  <h1 class="text-5xl mb-5">Главная</h1>
+  <router-link :to="{name: 'PostsList'}" class="no-underline hover:underline decoration-sky-500">Список постов</router-link>
+  <div class="mt-6">
+    Главная страница
+  </div>
 </template>
 
 <script>
--- src/pages/PostsList.vue
+++ src/pages/PostsList.vue
@@ -1,17 +1,20 @@
 <template>
-  <h1>Спискок постов</h1>
-  <router-link :to="{name: 'Main'}">Главная</router-link>
-  <div class="">
-    <div class="" v-for="post in postList">
-      <span>
+  <h1 class="text-5xl mb-5">Спискок постов</h1>
+  <router-link :to="{name: 'Main'}" class="no-underline hover:underline">Главная</router-link>
+  <div class="divide-y divide-blue-200 mt-4">
+    <div class="flex gap-1.5 pt-4 pb-4" v-for="post in postList">
+      <div class="flex-none w-10 ">
       {{ post.id }}
-      </span>
-      <div class="">
-        {{ post.title }}
       </div>
-      <div class="">
-        {{ post.body }}
+      <div class="flex-1 w-80">
+        <div class="text-3xl mb-3">
+          {{ post.title }}
+        </div>
+        <div class="">
+          {{ post.body }}
+        </div>
       </div>
+
     </div>
   </div>
 </template>
